0379dc3f4fffa8f06a9ddf083cbb6e785a82335d
jiayunyun <jy163lovefamily@163.com>
2020-02-12 19:51:54 +0800

    初始化项目

---
+++ src/main/java/com/jy/hello/spring/boot/thymeleaf/HelloSpringBootThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.jy.hello.spring.boot.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringBootThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/jy/hello/spring/boot/thymeleaf/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.jy.hello.spring.boot.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+    @RequestMapping(value = {"","index"},method = RequestMethod.GET)
+    public  String index(){
+
+        return "index";
+    }
+}
