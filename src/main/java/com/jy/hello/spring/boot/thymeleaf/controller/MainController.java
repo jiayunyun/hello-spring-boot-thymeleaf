a48a9d60545c345015c8d1fda4884f11d3ac1c89
jiayunyun <jy163lovefamily@163.com>
2020-02-13 09:09:55 +0800

    初始化项目

--- src/main/java/com/jy/hello/spring/boot/thymeleaf/controller/MainController.java
+++ src/main/java/com/jy/hello/spring/boot/thymeleaf/controller/MainController.java
@@ -1,14 +1,18 @@
 package com.jy.hello.spring.boot.thymeleaf.controller;
 
+import com.jy.hello.spring.boot.thymeleaf.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
     @RequestMapping(value = {"","index"},method = RequestMethod.GET)
-    public  String index(){
-
+    public  String index(Model model){
+        User user  = new User();
+        user.setName("贾云66666");
+        model.addAttribute("user",user);
         return "index";
     }
 }
